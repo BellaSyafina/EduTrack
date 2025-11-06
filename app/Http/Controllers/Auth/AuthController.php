c2e8e28fd0199219574cd5c9c887dce052f370fc
BellaSyafina <bellaarumsyafina04@gmail.com>
2025-11-06 23:02:42 +0700

    EduTrack

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        $data = [
+            'title' => 'login'
+        ];
+        return view('auth.login', $data);
+    }
+}
